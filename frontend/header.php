bd781a36da2b289df05c78912ae968882ff23bb9
imehedi007 <olivegreen.mehedi@gmail.com>
2024-01-28 12:29:51 +0600

    frontend

---
+++ frontend/header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Blog site</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+
+	<!-- STYLES -->
+	<link rel="stylesheet" href="../assets/frontend/css/bootstrap.min.css" type="text/css" media="all">
+	<link rel="stylesheet" href="../assets/frontend/css/all.min.css" type="text/css" media="all">
+	<link rel="stylesheet" href="../assets/frontend/css/slick.css" type="text/css" media="all">
+	<link rel="stylesheet" href="../assets/frontend/css/simple-line-icons.css" type="text/css" media="all">
+	<link rel="stylesheet" href="../assets/frontend/css/style.css" type="text/css" media="all">
+
+</head>
+<body>
+
+
+<!-- site wrapper -->
+<div class="site-wrapper">
+
+	<div class="main-overlay"></div>
+
+	<!-- header -->
+	<header class="header-classic">
+
+			<div class="container-xl">
+				<!-- header top -->
+				<div class="header-top">
+					<div class="row align-items-center">
+
+						<div class="col-md-4 col-xs-12">
+							<!-- site logo -->
+							<a class="navbar-brand" href="classic.html"><img src="images/logo.svg" alt="logo" /></a> 
+						</div>
+
+						<div class="col-md-8 d-none d-md-block">
+							<!-- social icons -->
+							<ul class="social-icons list-unstyled list-inline mb-0 float-end">
+								<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+								<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+								<li class="list-inline-item"><a href="#"><i class="fab fa-medium"></i></a></li>
+								<li class="list-inline-item"><a href="#"><i class="fab fa-youtube"></i></a></li>
+							</ul>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+			<nav class="navbar navbar-expand-lg">
+				<!-- header bottom -->
+				<div class="header-bottom  w-100">
+					
+					<div class="container-xl">
+						<div class="d-flex align-items-center">
+							<div class="collapse navbar-collapse flex-grow-1">
+								<!-- menus -->
+								<ul class="navbar-nav">
+									<li class="nav-item dropdown active">
+										<a class="nav-link" href="index.php">Home</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="about.php">About Us</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="category.html">Inspiration</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="./contact.php">Contact</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</nav>
+
+	</header>
+
+    
---
+++ frontend/index.php
@@ -0,0 +1,291 @@
+<?php
+	require './header.php';
+?>
+
+<section class="hero-carousel">
+        <div class="container-xl">
+            <div class="post-carousel-lg">
+                <!-- post -->
+                <div class="post featured-post-xl">
+                    <div class="details clearfix">
+                        <a href="category.html" class="category-badge lg">Lifestyle</a>
+                        <h4 class="post-title"><a href="blog-single.html">9 Most Awesome Blue Lake With Snow Mountain</a></h4>
+                        <ul class="meta list-inline mb-0">
+                            <li class="list-inline-item"><a href="#">Katen Doe</a></li>
+                            <li class="list-inline-item">29 March 2021</li>
+                        </ul>
+                    </div>
+                    <a href="blog-single.html">
+                        <div class="thumb rounded">
+                            <div class="inner data-bg-image" data-bg-image="images/posts/featured-xl-1.jpg"></div>
+                        </div>
+                    </a>
+                </div>
+                <!-- post -->
+                <div class="post featured-post-xl">
+                    <div class="details clearfix">
+                        <a href="category.html" class="category-badge lg">Culture</a>
+                        <h4 class="post-title"><a href="blog-single.html">Important Thing You Need To Know About Swim</a></h4>
+                        <ul class="meta list-inline mb-0">
+                            <li class="list-inline-item"><a href="#">Katen Doe</a></li>
+                            <li class="list-inline-item">29 March 2021</li>
+                        </ul>
+                    </div>
+                    <a href="blog-single.html">
+                        <div class="thumb rounded">
+                            <div class="inner data-bg-image" data-bg-image="images/posts/featured-xl-2.jpg"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+	<!-- section main content -->
+	<section class="main-content">
+		<div class="container-xl">
+
+			<div class="row gy-4">
+
+				<div class="col-lg-8">
+
+					<!-- post -->
+					<div class="post post-classic rounded bordered">
+						<div class="thumb top-rounded">
+							<a href="category.html" class="category-badge lg position-absolute">Lifestyle</a>
+							<a href="blog-single.html">
+								<div class="inner">
+									<img src="images/posts/post-lg-1.jpg" alt="post-title" />
+								</div>
+							</a>
+						</div>
+						<div class="details">
+							<ul class="meta list-inline mb-0">
+								<li class="list-inline-item"><a href="#"><img src="images/other/author-sm.png" class="author" alt="author"/>Katen Doe</a></li>
+								<li class="list-inline-item">29 March 2021</li>
+								<li class="list-inline-item"><i class="icon-bubble"></i> (0)</li>
+							</ul>
+							<h5 class="post-title mb-3 mt-3"><a href="blog-single.html">How To Become Better With Building In 1 Month</a></h5>
+							<p class="excerpt mb-0">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						</div>
+						<div class="post-bottom clearfix d-flex align-items-center">
+							<div class="social-share me-auto">
+								<button class="toggle-button icon-share"></button>
+								<ul class="icons list-unstyled list-inline mb-0">
+									<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-telegram-plane"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="far fa-envelope"></i></a></li>
+								</ul>
+							</div>
+							<div class="float-end d-none d-md-block">
+								<a href="blog-single.html" class="more-link">Continue reading<i class="icon-arrow-right"></i></a>
+							</div>
+							<div class="more-button d-block d-md-none float-end">
+								<a href="blog-single.html"><span class="icon-options"></span></a>
+							</div>
+						</div>
+					</div>
+
+					<!-- post -->
+					<div class="post post-classic rounded bordered">
+						<div class="thumb top-rounded">
+							<a href="category.html" class="category-badge lg position-absolute">Inspiration</a>
+							<a href="blog-single.html">
+								<div class="inner">
+									<img src="images/posts/post-lg-2.jpg" alt="post-title" />
+								</div>
+							</a>
+						</div>
+						<div class="details">
+							<ul class="meta list-inline mb-0">
+								<li class="list-inline-item"><a href="#"><img src="images/other/author-sm.png" class="author" alt="author"/>Katen Doe</a></li>
+								<li class="list-inline-item">29 March 2021</li>
+								<li class="list-inline-item"><i class="icon-bubble"></i> (0)</li>
+							</ul>
+							<h5 class="post-title mb-3 mt-3"><a href="blog-single.html">10 Ways To Immediately Start Selling Furniture</a></h5>
+							<p class="excerpt mb-0">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						</div>
+						<div class="post-bottom clearfix d-flex align-items-center">
+							<div class="social-share me-auto">
+								<button class="toggle-button icon-share"></button>
+								<ul class="icons list-unstyled list-inline mb-0">
+									<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-telegram-plane"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="far fa-envelope"></i></a></li>
+								</ul>
+							</div>
+							<div class="float-end d-none d-md-block">
+								<a href="blog-single.html" class="more-link">Continue reading<i class="icon-arrow-right"></i></a>
+							</div>
+							<div class="more-button d-block d-md-none float-end">
+								<a href="blog-single.html"><span class="icon-options"></span></a>
+							</div>
+						</div>
+					</div>
+
+					<!-- post -->
+					<div class="post post-classic rounded bordered">
+						<div class="thumb top-rounded">
+							<a href="category.html" class="category-badge lg position-absolute">Culture</a>
+							<a href="blog-single.html">
+								<div class="inner">
+									<img src="images/posts/post-lg-3.jpg" alt="post-title" />
+								</div>
+							</a>
+						</div>
+						<div class="details">
+							<ul class="meta list-inline mb-0">
+								<li class="list-inline-item"><a href="#"><img src="images/other/author-sm.png" class="author" alt="author"/>Katen Doe</a></li>
+								<li class="list-inline-item">29 March 2021</li>
+								<li class="list-inline-item"><i class="icon-bubble"></i> (0)</li>
+							</ul>
+							<h5 class="post-title mb-3 mt-3"><a href="blog-single.html">Most Important Thing You Need To Know About Swim</a></h5>
+							<p class="excerpt mb-0">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						</div>
+						<div class="post-bottom clearfix d-flex align-items-center">
+							<div class="social-share me-auto">
+								<button class="toggle-button icon-share"></button>
+								<ul class="icons list-unstyled list-inline mb-0">
+									<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-telegram-plane"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="far fa-envelope"></i></a></li>
+								</ul>
+							</div>
+							<div class="float-end d-none d-md-block">
+								<a href="blog-single.html" class="more-link">Continue reading<i class="icon-arrow-right"></i></a>
+							</div>
+							<div class="more-button d-block d-md-none float-end">
+								<a href="blog-single.html"><span class="icon-options"></span></a>
+							</div>
+						</div>
+					</div>
+
+					<!-- post -->
+					<div class="post post-classic rounded bordered">
+						<div class="thumb top-rounded">
+							<a href="category.html" class="category-badge lg position-absolute">Lifestyle</a>
+							<a href="blog-single.html">
+								<div class="inner">
+									<img src="images/posts/post-lg-4.jpg" alt="post-title" />
+								</div>
+							</a>
+						</div>
+						<div class="details">
+							<ul class="meta list-inline mb-0">
+								<li class="list-inline-item"><a href="#"><img src="images/other/author-sm.png" class="author" alt="author"/>Katen Doe</a></li>
+								<li class="list-inline-item">29 March 2021</li>
+								<li class="list-inline-item"><i class="icon-bubble"></i> (0)</li>
+							</ul>
+							<h5 class="post-title mb-3 mt-3"><a href="blog-single.html">Your Light Is About To Stop Being Relevant</a></h5>
+							<p class="excerpt mb-0">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						</div>
+						<div class="post-bottom clearfix d-flex align-items-center">
+							<div class="social-share me-auto">
+								<button class="toggle-button icon-share"></button>
+								<ul class="icons list-unstyled list-inline mb-0">
+									<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-telegram-plane"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="far fa-envelope"></i></a></li>
+								</ul>
+							</div>
+							<div class="float-end d-none d-md-block">
+								<a href="blog-single.html" class="more-link">Continue reading<i class="icon-arrow-right"></i></a>
+							</div>
+							<div class="more-button d-block d-md-none float-end">
+								<a href="blog-single.html"><span class="icon-options"></span></a>
+							</div>
+						</div>
+					</div>
+
+					<nav>
+						<ul class="pagination justify-content-center">
+							<li class="page-item active" aria-current="page">
+								<span class="page-link">1</span>
+							</li>
+							<li class="page-item"><a class="page-link" href="#">2</a></li>
+							<li class="page-item"><a class="page-link" href="#">3</a></li>
+						</ul>
+					</nav>
+
+				</div>
+				<div class="col-lg-4">
+
+					<!-- sidebar -->
+					<div class="sidebar">
+						<!-- widget about -->
+						<div class="widget rounded">
+							<div class="widget-about data-bg-image text-center" data-bg-image="images/map-bg.png">
+								<img src="images/logo.svg" alt="logo" class="mb-4" />
+								<p class="mb-4">Hello, We’re content writer who is fascinated by content fashion, celebrity and lifestyle. We helps clients bring the right content to the right people.</p>
+								<ul class="social-icons list-unstyled list-inline mb-0">
+									<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-medium"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-youtube"></i></a></li>
+								</ul>
+							</div>
+						</div>
+
+
+						<!-- widget categories -->
+						<div class="widget rounded">
+							<div class="widget-header text-center">
+								<h3 class="widget-title">Explore Topics</h3>
+								<img src="images/wave.svg" class="wave" alt="wave" />
+							</div>
+							<div class="widget-content">
+								<ul class="list">
+									<li><a href="#">Lifestyle</a><span>(5)</span></li>
+									<li><a href="#">Inspiration</a><span>(2)</span></li>
+									<li><a href="#">Fashion</a><span>(4)</span></li>
+									<li><a href="#">Politic</a><span>(1)</span></li>
+									<li><a href="#">Trending</a><span>(7)</span></li>
+									<li><a href="#">Culture</a><span>(3)</span></li>
+								</ul>
+							</div>
+							
+						</div>
+
+						<!-- widget newsletter -->
+						<div class="widget rounded">
+							<div class="widget-header text-center">
+								<h3 class="widget-title">Newsletter</h3>
+								<img src="images/wave.svg" class="wave" alt="wave" />
+							</div>
+							<div class="widget-content">
+								<span class="newsletter-headline text-center mb-3">Join 70,000 subscribers!</span>
+								<form>
+									<div class="mb-2">
+										<input class="form-control w-100 text-center" placeholder="Email address…" type="email">
+									</div>
+									<button class="btn btn-default btn-full" type="submit">Sign Up</button>
+								</form>
+								<span class="newsletter-privacy text-center mt-3">By signing up, you agree to our <a href="#">Privacy Policy</a></span>
+							</div>		
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+	</section>
+
+<?php
+	require './footer.php';
+?>
